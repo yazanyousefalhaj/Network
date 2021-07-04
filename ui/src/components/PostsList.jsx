8674651c7d7ab257a850f9c8004fb5cbc10ce6c5
yazanyousefalhaj <yazanyousefalhaj@gmail.com>
2021-07-04 11:19:05 +0300

    extract post component

--- ui/src/components/PostsList.jsx
+++ ui/src/components/PostsList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from "react"
 import { authContext } from "../authContext.jsx"
+import { Post } from './Post.jsx'
 
 
 export const PostList = ({ list }) => {
@@ -9,42 +10,7 @@ export const PostList = ({ list }) => {
 				list ? (
 					<div className="container posts-content">
 						<div className="row">
-							{list.map((post) => (
-								<div className="col-lg-6" key={post.id}>
-									<div className="card mb-4">
-
-										<div className="card-body">
-											<div className="media mb-3">
-												<img 
-													src="https://bootdey.com/img/Content/avatar/avatar3.png" 
-													className="d-block ui-w-40 rounded-circle" 
-													alt="" 
-												/>
-												<div className="media-body ml-3">
-													{post.author_name}
-													<div className="text-muted small">3 days ago</div>
-												</div>
-											</div>
-											<p>{post.body}</p>
-										</div>
-
-										<div className="card-footer">
-											<a href="#" className="d-inline-block text-muted">
-												<strong>{post.likes}</strong>
-												<small>Likes</small>
-											</a>
-											<a href="#" className="d-inline-block text-muted ml-3">
-												<strong>12</strong>
-												<small>Comments</small>
-											</a>
-											<a href="#" className="d-inline-block text-muted ml-3">
-												<small className="align-middle">Repost</small>
-											</a>
-										</div>
-
-									</div>
-								</div>
-							))}
+							{list.map((post) => <Post post={post} key={post.id} /> )}
 						</div>
 					</div>
 				) : "No Posts"
