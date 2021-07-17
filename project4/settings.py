a21b2d8be8a96b3638815c910c553c6894e7f5de
yazanyousefalhaj <yazanyousefalhaj@gmail.com>
2021-07-17 21:12:40 +0300

    dockerize and use postgresql

--- project4/settings.py
+++ project4/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'project4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
